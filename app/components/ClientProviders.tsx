996f19cd7941d0c6aa851dc29d64042932cf6d15
Marhk <majekmarhkson@gmail.com>
2026-05-12 02:04:31 +0000

    fix: use WalletAdapterNetwork.Testnet enum

--- app/components/ClientProviders.tsx
+++ app/components/ClientProviders.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React from "react";
-import { MidenFiSignerProvider } from "@miden-sdk/miden-wallet-adapter";
+import { MidenFiSignerProvider, WalletAdapterNetwork } from "@miden-sdk/miden-wallet-adapter";
 import { MidenProvider } from "@miden-sdk/react";
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
   return (
-    <MidenFiSignerProvider appName="Shado" network="testnet">
+    <MidenFiSignerProvider appName="Shado" network={WalletAdapterNetwork.Testnet}>
       <MidenProvider config={{ rpcUrl: "testnet" }}>
         {children}
       </MidenProvider>
